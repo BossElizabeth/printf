4bbdcb1303546ac259997151120be6f9b23e1c09
chux-da-xteevn <chukwu.steevn@gmail.com>
2023-11-14 14:49:53 -0800

    task 111

--- _printf.c
+++ _printf.c
@@ -1,73 +1,53 @@
-#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
 
 /**
- * _printf - to emulate the real printf function.
- * @format: Formated string.
- *
- * Return: shows how many characters printed.
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-
-	while (*format != '\0')
+	if (format != NULL)
 	{
-		if (*format == '%' && *(format + 1) != '\0')
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
+
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
 		{
-			format++;
-			switch (*format)
+			if (format[i] == '%')
 			{
-			case 'c':
-				count += putchar(va_arg(args, int));
-				break;
-			case 's':
-				count += _printstr(va_arg(args, char *));
-				break;
-			case '%':
-				count += putchar('%');
-				break;
-			default:
-				count += putchar('%');
-				count += putchar(*format);
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
 			}
 		}
-		else
-		{
-			count += putchar(*format);
-		}
-
-		format++;
+		va_end(args);
+		return (count);
 	}
-
-	va_end(args);
-
-	return (count);
-}
-
-/**
- * _printstr - combine characters to produce a string.
- * @str: String to display.
- *
- * Return: how many characters printed.
- */
-int _printstr(const char *str)
-{
-	int count = 0;
-
-	if (str == NULL)
-		str = "(null)";
-
-	while (*str != '\0')
-	{
-		count += putchar(*str);
-		str++;
-	}
-
-	return (count);
+	return (-1);
 }
--- main.h
+++ main.h
@@ -1,7 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
-int _printstr(const char *str);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char v))(va_list args);
 
 #endif
